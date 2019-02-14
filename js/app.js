a41d634e14e5e4ab30503c57fb24267a8879d6b1
Wilder Ledes <wilder@ledes.me>
2019-02-14 00:59:08 -0500

    Added: tile shuffling

--- js/app.js
+++ js/app.js
@@ -1,4 +1,23 @@
 const board = document.querySelector('.board');
+const cards = document.querySelectorAll('.card');
+
+function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
+const nums = shuffle([...Array(cards.length).keys()]);
+
+// Applies shuffled order to DOM
+function orderShuffle(cardList, nums) {
+    for (let i = 0; i < cardList.length; i++) {
+        cardList[i].setAttribute('style', 'order: ' + nums[i]);
+    }
+}
+
+orderShuffle(cards, nums);
 
 // Main listener function for tile clicks
 function tileClick(evt) {
